17fe2f8f2530c61f78e316ced16c0b925855b384
Lan <72094879+jianglanni@users.noreply.github.com>
2021-04-09 15:25:19 +0800

    Add files via upload

---
+++ tilepuzzle.py
@@ -0,0 +1,150 @@
+# Lan Jiang
+# ECS170
+# HW1
+# Please give the program enough time (like 60 seconds) to run, thanks!
+
+visited = []
+
+# To check if a state has been visited
+def isVisited(status):
+    for ins in visited:
+        if ins == status:
+            return True
+    return False
+
+# This function generates the new states based on the current state
+def newStatus(current):
+    ret = []
+    col = -1
+    row = -1
+    # Find where the hole is
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if current[i][j] == 0:
+                row = i
+                col = j
+    if row > 0:
+        temp = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+        current[row][col], current[row - 1][col] = current[row - 1][col], current[row][col]
+        for i in range(0, 3):
+            for j in range(0, 3):
+                temp[i][j] = current[i][j]
+        if not isVisited(temp):
+            ret.append(temp)
+        current[row][col], current[row - 1][col] = current[row - 1][col], current[row][col]
+
+    if row < 2:
+        temp = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+        current[row][col], current[row + 1][col] = current[row + 1][col], current[row][col]
+        for i in range(0, 3):
+            for j in range(0, 3):
+                temp[i][j] = current[i][j]
+        if not isVisited(temp):
+            ret.append(temp)
+        current[row][col], current[row + 1][col] = current[row + 1][col], current[row][col]
+
+    if col > 0:
+        temp = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+        current[row][col], current[row][col - 1] = current[row][col - 1], current[row][col]
+        for i in range(0, 3):
+            for j in range(0, 3):
+                temp[i][j] = current[i][j]
+        if not isVisited(temp):
+            ret.append(temp)
+        current[row][col], current[row][col - 1] = current[row][col - 1], current[row][col]
+
+    if col < 2:
+        temp = [[-1,-1,-1], [-1,-1,-1], [-1,-1,-1]]
+        current[row][col], current[row][col + 1] = current[row][col + 1], current[row][col]
+        for i in range(0, 3):
+            for j in range(0, 3):
+                temp[i][j] = current[i][j]
+        if not isVisited(temp):
+            ret.append(temp)
+        current[row][col], current[row][col + 1] = current[row][col + 1], current[row][col]
+    return ret
+
+# Try to find an answer quickly by giving different depth limits to the searches
+def tilepuzzle(initial, goal):
+    ret = tilePath(initial, goal, [], 0, 5)[::-1]
+    if ret:
+        return ret
+    visited.clear()
+
+    ret = tilePath(initial, goal, [], 0, 10)[::-1]
+    if ret:
+        return ret
+    visited.clear()
+
+    ret = tilePath(initial, goal, [], 0, 33)[::-1]
+    if ret:
+        return ret
+    visited.clear()
+
+    ret = tilePath(initial, goal, [], 0, 50)[::-1]
+    if ret:
+        return ret
+    visited.clear()
+
+    ret = tilePath(initial, goal, [], 0, 100)[::-1]
+    if ret:
+        return ret
+    visited.clear()
+
+    print("This puzzle cannot be solved in 100 steps.")
+    return []
+
+
+def tilePath(current, goal, path, depth, limit):
+    if current == goal:
+        path.append(current)
+        return path
+
+    # Add current state to the visited state list
+    current_visit = [[-1,-1,-1], [-1,-1,-1], [-1,-1,-1]]
+    for i in range(0, 3):
+        for j in range(0, 3):
+            current_visit[i][j] = current[i][j]
+    visited.append(current_visit)
+
+    # generate a list that contains the next possible moves from the current state
+    moves = newStatus(current)
+
+    # Dead end
+    if moves == []:
+        visited.pop(-1)
+        return []
+
+    # Too deep, escape!
+    if depth > limit:
+        visited.pop(-1)
+        return []
+
+    # Peek ahead
+    for nextMove in moves:
+        if nextMove == goal:
+            path.append(goal)
+            path.append(current)
+            return path
+
+    # Recursive, deeper search
+    for nextMove in moves:
+        temp = tilePath(nextMove, goal, path, depth+1, limit)
+        if temp != [] and temp[0] == goal:
+            path.append(current)
+            return path
+
+    # All "nextmoves" go to dead ends
+    visited.pop(-1)
+    return []
+
+# Subject to change
+def main():
+    container = tilepuzzle([[0,1,2],[3,4,5],[6,7,8]], [[8,7,6],[5,4,3],[2,1,0]])
+    for e in container:
+        print(e)
+    print(len(container))
+
+
+main()
+#print(tilepuzzle([[1, 2, 3], [8, 0, 4], [7, 6, 5]], [[2, 8, 3], [1, 0, 4], [7, 6, 5]]))
