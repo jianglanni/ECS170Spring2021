c39ecf6099bcd72bc1c153ed846fdbb4f33f3fcc
Lan <72094879+jianglanni@users.noreply.github.com>
2021-04-14 08:27:42 +0800

    backup for changing

--- hw1/tilepuzzle.py
+++ hw1/tilepuzzle.py
@@ -2,6 +2,65 @@
 # ECS170
 # HW1
 # Please give the program enough time (like 60 seconds) to run, thanks!
+# Feel free to modify the depth_list on Line 10
+
+
+# Try to find an answer quickly by giving different depth limits to the searches
+def tilepuzzle(initial, goal):
+    depth_list = [5, 10, 30] # Three different depth limits
+    for depth_limit in depth_list:
+        ret = statesearch(initial, goal, [], 0, depth_limit)[::-1]
+        if ret:
+            return ret
+        visited.clear()
+
+    print("This puzzle cannot be solved in limited steps.")
+    return []
+
+
+def statesearch(current, goal, path, depth, limit):
+    if current == goal:
+        path.append(current)
+        return path
+
+    # Add current state to the visited state list
+    current_visit = [[-1,-1,-1], [-1,-1,-1], [-1,-1,-1]]
+    for i in range(0, 3):
+        for j in range(0, 3):
+            current_visit[i][j] = current[i][j]
+    visited.append(current_visit)
+
+    # generate a list that contains the next possible moves from the current state
+    moves = newStatus(current)
+
+    # Dead end
+    if moves == []:
+        visited.pop(-1)
+        return []
+
+    # Too deep, escape!
+    if depth > limit:
+        visited.pop(-1)
+        return []
+
+    # Peek ahead
+    for nextMove in moves:
+        if nextMove == goal:
+            path.append(goal)
+            path.append(current)
+            return path
+
+    # Recursive, deeper search (DFS)
+    for nextMove in moves:
+        temp = statesearch(nextMove, goal, path, depth+1, limit)
+        if temp != [] and temp[0] == goal:
+            path.append(current)
+            return path
+
+    # All "nextmoves" go to dead ends
+    visited.pop(-1)
+    return []
+
 
 visited = []
 
@@ -64,66 +123,9 @@ def newStatus(current):
     return ret
 
 
-# Try to find an answer quickly by giving different depth limits to the searches
-def tilepuzzle(initial, goal):
-    depth_list = [5, 10, 33]
-    for depth in depth_list:
-        ret = tilePath(initial, goal, [], 0, depth)[::-1]
-        if ret:
-            return ret
-        visited.clear()
-
-    print("This puzzle cannot be solved in 33 steps.")
-    return []
-
-
-def tilePath(current, goal, path, depth, limit):
-    if current == goal:
-        path.append(current)
-        return path
-
-    # Add current state to the visited state list
-    current_visit = [[-1,-1,-1], [-1,-1,-1], [-1,-1,-1]]
-    for i in range(0, 3):
-        for j in range(0, 3):
-            current_visit[i][j] = current[i][j]
-    visited.append(current_visit)
-
-    # generate a list that contains the next possible moves from the current state
-    moves = newStatus(current)
-
-    # Dead end
-    if moves == []:
-        visited.pop(-1)
-        return []
-
-    # Too deep, escape!
-    if depth > limit:
-        visited.pop(-1)
-        return []
-
-    # Peek ahead
-    for nextMove in moves:
-        if nextMove == goal:
-            path.append(goal)
-            path.append(current)
-            return path
-
-    # Recursive, deeper search
-    for nextMove in moves:
-        temp = tilePath(nextMove, goal, path, depth+1, limit)
-        if temp != [] and temp[0] == goal:
-            path.append(current)
-            return path
-
-    # All "nextmoves" go to dead ends
-    visited.pop(-1)
-    return []
-
-
-# test case
-start = [[0,1,2],[3,4,5],[6,7,8]]
-end = [[8,7,6],[5,4,3],[0,2,1]]
+# My own test case
+start = [[2,5,7],[8,0,6],[1,4,3]]
+end = [[1,2,3],[4,5,6],[7,8,0]]
 answer = tilepuzzle(start, end)
 for e in answer:
     print(e)
