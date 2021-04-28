bededb00eb6052b6031712dc480126e0d09e0a61
Lan <72094879+jianglanni@users.noreply.github.com>
2021-04-28 13:17:10 +0800

    Bug fixes

--- hw2.py
+++ hw2.py
@@ -10,11 +10,43 @@ frontier = []  # (f, g, state), a heap priority queue
 prev = {}  # to save the route
 
 
+# Blocking heuristic
+def h_block(state_str):
+    if state_str[coord(2, 5)] == 'X':
+        return 0  # goal state reached
+    mat = str_to_mat(state_str)
+    vehicle_block = 0
+    for i in range(0, 6)[::-1]:
+        if mat[2][i] == 'X':
+            return vehicle_block + 1  # X car detected, return the value
+        elif mat[2][i] != '-':
+            vehicle_block = vehicle_block + 1  # vehicle detected
+    assert 0  # No X car found, escape
+
+
+# updating heuristic
+def m_block(state_str):
+    if state_str[coord(2, 5)] == 'X':
+        return 0  # goal state reached
+    vehicle_block = 0
+    for i in range(0, 6)[::-1]:
+        if state_str[coord(2, i)] == 'X':
+            return vehicle_block + 1  # X car detected, return the value
+        elif state_str[coord(2, i)] != '-':
+            vehicle_block += 1  # vehicle detected
+            if state_str[coord(2, i)] == state_str[coord(1, i)] and state_str[coord(2, i)] == state_str[coord(3, i)]:
+                vehicle_block += 1
+            else:
+                if state_str[coord(2, i)] == state_str[coord(1, i)] and state_str[coord(2, i)] == state_str[coord(0, i)]:
+                    vehicle_block += 2
+    assert 0  # No X car found, escape
+
+
 # Find a state in frontier
 def frontier_search(state_str):
     ret = -1
     for i in range(0, len(frontier)):
-        if state_str == frontier[i]:
+        if state_str == frontier[i][2]:
             ret = i
             break
     return ret
@@ -37,36 +69,35 @@ def rushhour_astar(heu, state_str):
     if heu == 0:
         heapq.heappush(frontier, (h_block(state_str), 0, state_str))
     else:
-        heapq.heappush(frontier, (0, 0, state_str))
+        heapq.heappush(frontier, (m_block(state_str), 0, state_str))
     prev[state_str] = ""
     #  A* algorithm starts here
     while len(frontier):
-        state_tuple = heapq.heappop(frontier)
-        visited.append(state_tuple[2])
+        state_tuple = heapq.heappop(frontier)  # Getting the state with smallest f
+        if is_goal(state_tuple[2]):
+            return state_tuple[2]
+        visited.append(state_tuple[2])  # Mark as visited
         next_steps = new_moves(state_tuple[2])
         for step in next_steps:
             if step in visited:
                 continue
-            if is_goal(step):
-                prev[step] = state_tuple[2]
-                return step
             cur_g = state_tuple[1] + 1 # new g value
             cur_f = cur_g
             if heu == 0:  # Given heuristic
                 cur_f += h_block(state_tuple[2])
             else:  # My heuristic
-                cur_f += 0
-            if frontier_search(step) != -1:  # Detected this state in frontier already
-                temp_loc = frontier_search(step)
+                cur_f += m_block(state_tuple[2])
+            temp_loc = frontier_search(step)  # Check if this state is already in frontier
+            if temp_loc != -1:
                 if cur_g < frontier[temp_loc][1]:  # Current g is smaller than existing g
                     frontier[temp_loc] = (cur_f, cur_g, step)  # renew the frontier
-                    heapq.heapify(frontier)  # Keep the frontier in order
+                    heapq.heapify(frontier)  # Sort the frontier
                     prev[step] = state_tuple[2]  # Route update
             else:
                 prev[step] = state_tuple[2]  # Route
                 heapq.heappush(frontier, (cur_f, cur_g, step))  # Update frontier
     print("Failed")
-    return ""
+    return ""  # Failed return
 
 
 # matrix (list) to string
@@ -85,27 +116,14 @@ def str_to_mat(rows):
     return ret
 
 
-# Quick, direct access to string
+# Coordinate converter
 def coord(x, y):
     return 6 * x + y
 
 
+# Goal check
 def is_goal(state_str):
     return state_str[coord(2, 5)] == 'X'
-
-
-# Given heuristic
-def h_block(state_str):
-    if state_str[coord(2, 5)] == 'X':
-        return 0  # goal state reached
-    mat = str_to_mat(state_str)
-    vehicle_block = 0
-    for i in range(0, 6)[::-1]:
-        if mat[2][i] == 'X':
-            return vehicle_block + 1  # X car detected, return the value
-        elif mat[2][i] != '-':
-            vehicle_block = vehicle_block + 1 # vehicle detected
-    assert 0  # No X car found
 
 
 # New states generator
@@ -113,34 +131,24 @@ def new_moves(state_str):
     ret = []
     for i in range(0, 6):
         for j in range(0, 6):
-            temp = car_left(state_str, i, j)
-            if temp:
-                ret.append(temp)
             temp = car_right(state_str, i, j)
-            if temp:
-                ret.append(temp)
-            temp = car_up(state_str, i, j)
             if temp:
                 ret.append(temp)
             temp = car_down(state_str, i, j)
             if temp:
                 ret.append(temp)
-            temp = truck_left(state_str, i, j)
+            temp = car_left(state_str, i, j)
             if temp:
                 ret.append(temp)
-            temp = truck_right(state_str, i, j)
-            if temp:
-                ret.append(temp)
-            temp = truck_up(state_str, i, j)
-            if temp:
-                ret.append(temp)
-            temp = truck_down(state_str, i, j)
+            temp = car_up(state_str, i, j)
             if temp:
                 ret.append(temp)
     return tuple(ret)
 
 
+# Vehicle goes left
 def car_left(state_str, i, j):
+    is_truck = False
     mat = str_to_mat(state_str)
     if mat[i][j] == '-':
         return []
@@ -151,15 +159,20 @@ def car_left(state_str, i, j):
         return []
     if 0 < j < 4:  # It is a truck, not a car
         if mat[i][j] == mat[i][j+2]:
-            return []
+            is_truck = True
     mat[i][j-1] = mat[i][j]
-    mat[i][j+1] = '-'
+    if is_truck:
+        mat[i][j+2] = '-'
+    else:
+        mat[i][j+1] = '-'
     mat[i] = ''.join(mat[i])
     ret = mat_to_str(mat)
     return ret
 
 
+# Vehicle goes right
 def car_right(state_str, i, j):
+    is_truck = False
     mat = str_to_mat(state_str)
     if mat[i][j] == '-':
         return []
@@ -170,15 +183,20 @@ def car_right(state_str, i, j):
         return []
     if 1 < j < 5:
         if mat[i][j] == mat[i][j-2]:  # Truck detected
-            return []
+            is_truck = True
     mat[i][j+1] = mat[i][j]
-    mat[i][j-1] = '-'
+    if is_truck:
+        mat[i][j-2] = '-'
+    else:
+        mat[i][j-1] = '-'
     mat[i] = ''.join(mat[i])
     ret = mat_to_str(mat)
     return ret
 
 
+# Vehicle goes up
 def car_up(state_str, i, j):
+    is_truck = False
     mat = str_to_mat(state_str)
     if mat[i][j] == '-':
         return []
@@ -188,18 +206,23 @@ def car_up(state_str, i, j):
         return []
     if 0 < i < 4:  # it is a truck
         if mat[i][j] == mat[i+2][j]:
-            return []
+            is_truck = True
     for k in range(0, 6):
         mat[k] = list(mat[k])
     mat[i-1][j] = mat[i][j]
-    mat[i+1][j] = '-'
+    if is_truck:
+        mat[i+2][j] = '-'
+    else:
+        mat[i+1][j] = '-'
     for k in range(0, 6):
         mat[k] = ''.join(mat[k])
     ret = mat_to_str(mat)
     return ret
 
 
+# Vehicle goes down
 def car_down(state_str, i, j):
+    is_truck = False
     mat = str_to_mat(state_str)
     if mat[i][j] == '-':
         return []
@@ -209,79 +232,14 @@ def car_down(state_str, i, j):
         return []
     if 1 < i < 5:  # it is a truck
         if mat[i][j] == mat[i-2][j]:
-            return []
+            is_truck = True
     for k in range(0, 6):
         mat[k] = list(mat[k])
     mat[i+1][j] = mat[i][j]
-    mat[i-1][j] = '-'
-    for k in range(0, 6):
-        mat[k] = ''.join(mat[k])
-    ret = mat_to_str(mat)
-    return ret
-
-
-def truck_left(state_str, i, j):
-    mat = str_to_mat(state_str)
-    if mat[i][j] == '-':
-        return []
-    mat[i] = list(mat[i])
-    if j == 0 or j > 3:
-        return []
-    if not (mat[i][j] == mat[i][j+1] and mat[i][j] == mat[i][j+2]) or mat[i][j-1] != '-':
-        return []
-    mat[i][j-1] = mat[i][j]
-    mat[i][j+2] = '-'
-    mat[i] = ''.join(mat[i])
-    ret = mat_to_str(mat)
-    return ret
-
-
-def truck_right(state_str, i, j):
-    mat = str_to_mat(state_str)
-    if mat[i][j] == '-':
-        return []
-    mat[i] = list(mat[i])
-    if j == 5 or j < 2:
-        return []
-    if not (mat[i][j] == mat[i][j-1] and mat[i][j] == mat[i][j-2]) or mat[i][j+1] != '-':
-        return []
-    mat[i][j+1] = mat[i][j]
-    mat[i][j-2] = '-'
-    mat[i] = ''.join(mat[i])
-    ret = mat_to_str(mat)
-    return ret
-
-
-def truck_up(state_str, i, j):
-    mat = str_to_mat(state_str)
-    if mat[i][j] == '-':
-        return []
-    if i == 0 or i > 3:
-        return []
-    if not (mat[i][j] == mat[i+1][j] and mat[i][j] == mat[i+2][j]) or mat[i-1][j] != '-':
-        return []
-    for k in range(0, 6):
-        mat[k] = list(mat[k])
-    mat[i-1][j] = mat[i][j]
-    mat[i+2][j] = '-'
-    for k in range(0, 6):
-        mat[k] = ''.join(mat[k])
-    ret = mat_to_str(mat)
-    return ret
-
-
-def truck_down(state_str, i, j):
-    mat = str_to_mat(state_str)
-    if mat[i][j] == '-':
-        return []
-    if i == 5 or i < 2:
-        return []
-    if not (mat[i][j] == mat[i-1][j] and mat[i][j] == mat[i-2][j]) or mat[i+1][j] != '-':
-        return []
-    for k in range(0, 6):
-        mat[k] = list(mat[k])
-    mat[i+1][j] = mat[i][j]
-    mat[i-2][j] = '-'
+    if is_truck:
+        mat[i-2][j] = '-'
+    else:
+        mat[i-1][j] = '-'
     for k in range(0, 6):
         mat[k] = ''.join(mat[k])
     ret = mat_to_str(mat)
@@ -295,6 +253,11 @@ def print_boi(mat):
     print("")
 
 
+# My test cases
+state1 = ["--AABB","--CDEF","XXCDEF","--GGHH","------","------"]
 state2 = ["--B---","--B---","XXB---","--AA--","------","------"]
 state3 = ["--A---","--ABBB","--XXC-","----C-","----D-","----D-"]
-rushhour(0, state3)
+state4 = ["---A--","---A--","XX-A--","BCCC--","B-----","B-----"]
+state5 = ["GBB-L-","GHI-LM","GHIXXM","CCCK-M","--JKDD","EEJFF-"]
+state6 = ["-AA---", "-BBB-C", "XX---C", "---D-C", "---D--", "------"]
+rushhour(0, state1)
